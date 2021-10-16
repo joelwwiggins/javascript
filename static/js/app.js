a217be3fa7327bcb0f948e294b4870d9a1d4e1b6
Joel Wiggins <joelwwiggins@gmail.com>
2021-10-16 09:52:47 -0500

    get data into a table

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,17 @@
 // from data.js
 var tableData = data;
+var tbody=d3.select('tbody');
 
+function maketable(report){
+    d3.select('tbody').html("");
+    report.foreach(function (record){
+var row=tbody.append("tr");
+Object.entries(record).forEach(function ([key,value]){
+    var cell =row.append("td");
+    cell.text(value);
+});
+
+    });
+
+}
 // YOUR CODE HERE!
