549db22c74cb5daaa504422ffbbdac9d5e4ef4a4
Joel Wiggins <joelwwiggins@gmail.com>
2021-10-17 20:04:48 -0500

    found the capitalization errorgit add .git add .

--- static/js/app.js
+++ static/js/app.js
@@ -1,17 +1,44 @@
-// from data.js
-var tableData = data;
+//from data.js
+
+
+//get the data into the model
+var tableinfo= data;
+
+
+// set up the html body to modify
 var tbody=d3.select('tbody');
 
-function maketable(report){
-    d3.select('tbody').html("");
-    report.foreach(function (record){
-var row=tbody.append("tr");
-Object.entries(record).forEach(function ([key,value]){
-    var cell =row.append("td");
-    cell.text(value);
-});
+// start building the table
+function table1(tableininfo){
 
-    });
+//start with no data
+tbody.html('')
 
-}
-// YOUR CODE HERE!
+// loop through the data
+tableinfo.forEach((dataRow)=>{
+
+// append rows to the table
+    var row=tbody.append("tr");
+
+// loop through each row and add each value
+    Object.values(dataRow).forEach((value)=>{
+        var cell=row.append("td");
+        cell.text(value);
+    })
+})
+
+};
+table1(tableinfo)
+
+var button=d3.select("#filter-btn");
+button.on('click', function(){
+d3.event.preventDefault();
+ var input = d3.select('#datetime').property(value);
+ var filteredData = tableinfo.filter(row=> row.datetime ===input);
+ table1(filteredData);
+
+
+ });
+
+
+
